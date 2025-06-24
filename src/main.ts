9a313b438b88e56e7f3111bc72b0216d16cffd51
Adan-Perez <itsadandev@gmail.com>
2025-06-24 20:04:16 +0100

    feat: integrate OhVueIcons and register globally for improved icon usage

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,55 @@ import './assets/base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { 
+  HiHome, 
+  HiUsers, 
+  HiViewGrid, 
+  HiShoppingBag,
+  HiPlus,
+  HiPencil,
+  HiTrash,
+  HiEye,
+  HiRefresh,
+  HiSearch,
+  HiChevronDown,
+  HiChevronUp,
+  HiExclamationCircle,
+  HiShoppingCart,
+  HiArrowLeft,
+  HiStar,
+  HiCalendar
+} from 'oh-vue-icons/icons'
 
 import App from './App.vue'
 import router from './router'
 
+// Add icons to the library
+addIcons(
+  HiHome, 
+  HiUsers, 
+  HiViewGrid, 
+  HiShoppingBag,
+  HiPlus,
+  HiPencil,
+  HiTrash,
+  HiEye,
+  HiRefresh,
+  HiSearch,
+  HiChevronDown,
+  HiChevronUp,
+  HiExclamationCircle,
+  HiShoppingCart,
+  HiArrowLeft,
+  HiStar,
+  HiCalendar
+)
+
 const app = createApp(App)
+
+// Register OhVueIcon component globally
+app.component('v-icon', OhVueIcon)
 
 app.use(createPinia())
 app.use(router)
