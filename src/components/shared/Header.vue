c85ad5a16093e6aa2e382681bbbed428867f001f
JoseDev <joseslopez08k@gmail.com>
2025-07-02 19:44:39 +0200

    Fix: Make 'Categories' button navigate correctly
    Wraps the 'Categories' menu item in a <router-link> to /categories.
    
    Ensures the entire button area is clickable and consistent with other navigation links.
    
    Improves navigation UX in MainHeader.vue.

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -31,16 +31,16 @@
           <li class="active block p-3 duration-300 hover:bg-blue-200 lg:p-0 hover:lg:bg-transparent hover:lg:text-black">
             <router-link to="/">Inicio</router-link>
           </li>
-          <li class="flex cursor-pointer items-center justify-between p-3 hover:bg-blue-200 lg:gap-[2px] lg:p-0 hover:lg:bg-transparent hover:lg:text-black">
-            <router-link to="/categories">Categorias</router-link>
-            <span>
-              <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+          <li class="flex items-center justify-between p-3 hover:bg-blue-200 lg:gap-[2px] lg:p-0 hover:lg:bg-transparent hover:lg:text-black">
+            <router-link to="/categories" class="flex items-center w-full">
+              <span>Categorias</span>
+              <svg class="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20">
                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
               </svg>
-            </span>
+            </router-link>
           </li>
           <li class="block p-3 duration-300 hover:bg-blue-200 lg:p-0 hover:lg:bg-transparent hover:lg:text-black">
-            <router-link to="/about" class="block">
+            <router-link to="/about">
               Acerca de
             </router-link>
           </li>
