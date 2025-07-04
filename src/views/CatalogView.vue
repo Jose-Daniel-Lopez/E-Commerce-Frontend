bda5b2c597676f1c2945ef60ca977870b17f0ad7
JoseDev <joseslopez08k@gmail.com>
2025-07-04 20:56:31 +0200

    Lokete estoy cansao de escribir XD he añadio un catch por ahi pa un router.push :temecuidas:

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -283,7 +283,9 @@ const viewCategoryProducts = (categoryId: number) => {
 
     router.push(`/catalog/${categoryName}`).then(() => {
       window.scrollTo({ top: 0, behavior: 'smooth' })
-   })
+   }).catch(error => {
+      console.error('Error navigating to category:', error)
+    })
   }
 }
 
