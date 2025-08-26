efeb97d352fe99bdd3528e90d19dd8e17bbc1ed3
JoseDev <joseslopez08k@gmail.com>
2025-08-26 19:36:10 +0200

    fix: remove unused "Ver Más..." link from ViewMoreCard component

--- src/components/shared/ViewMoreCard.vue
+++ src/components/shared/ViewMoreCard.vue
@@ -3,13 +3,6 @@
     class="mb-4 ml-[2px] h-auto w-[163.5px] rounded-[9px] px-3 py-6 duration-500 hover:scale-[1.02] xs:w-[190px] sm:mb-0 sm:w-[298px] md:h-[435px] md:w-[240px] md:px-4 xl:w-[268px]"
   >
     <div class="flex h-[355px] w-full flex-col items-center justify-center gap-4 sm:h-[330px] md:h-[390px]">
-      <a
-        href="#"
-        @click.prevent
-        class="font-sm flex h-12 w-full items-center justify-center rounded-md border theme-border px-5 font-srProDisplay font-medium hover:button-primary sm:w-[183px] transition-colors duration-200"
-      >
-        Ver Más...
-      </a>
     </div>
   </div>
 </template>
