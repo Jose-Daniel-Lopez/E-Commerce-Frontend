ab51be2f8be94e02c7e6704e2215fed9eaa2ac7b
Jose Daniel <joseslopez08k@gmail.com>
2025-09-01 12:55:56 +0000

    feat: set Playwright to run tests in headless mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     trace: 'on-first-retry',
 
     /* Run tests in headed mode (visible browser) */
-    headless: false,
+    headless: true,
   },
 
   /* Configure projects for major browsers */
